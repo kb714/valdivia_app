396ec01a266fcef2a9b58e3829d2740a08adaab4
Alexis Aguirre <alexis@acvp.cl>
2017-11-13 00:44:39 -0300

    update app

--- app/controllers/admin/answers_controller.rb
+++ app/controllers/admin/answers_controller.rb
@@ -15,11 +15,31 @@ module Admin
       puts @all_data
     end
 
+    def edit
+      @data = DataStore.find_by_id(params[:id])
+    end
+
+    def update
+      to_update = {}
+      update_params.each do |item, value|
+        to_update[item] = value
+      end
+      @data = DataStore.find_by_id(params[:id])
+      @data.surveys = to_update
+      render :edit if @data.save
+    end
+
     def destroy
       @data = DataStore.find_by(id: params[:id])
       survey_id = @data.survey_id
       @data.destroy
       redirect_to admin_answer_path(survey_id), notice: 'Acción completada con éxito'
+    end
+
+    private
+
+    def update_params
+      params.require(:survey)
     end
   end
 end
